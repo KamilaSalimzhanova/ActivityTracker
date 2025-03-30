596ff9eb415b887f2713da488be1b1e94abd6914
kamila <ksalimzh@gmail.com>
2025-03-30 21:24:42 +0500

    Initial Commit

---
+++ ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by kamila on 30.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
